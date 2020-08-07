daa6f367335b7fc85b8c9a56bf13cfb5d1233c73
git <asyarb@hawaii.edu>
2020-08-07 09:28:00 -1000

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+use qstring::QString;
+
+// #[derive(Clone)]
+// pub enum ImgixFit {
+//     Clamp,
+//     Clip,
+//     Crop,
+//     FaceArea,
+//     Fill,
+//     FillMax,
+//     Max,
+//     Min,
+//     Scale,
+// }
+
+// #[derive(Clone)]
+// pub enum ImgixColorSpace {
+//     SRGB,
+//     AdobeRGB1998,
+//     TinySRGB,
+//     Strip,
+// }
+
+// #[derive(Builder, Clone)]
+// pub struct ImgixAuto {
+//     pub compress: bool,
+//     pub enhance: bool,
+//     pub format: bool,
+//     pub redeye: bool,
+// }
+
+// #[derive(Clone)]
+// pub enum RectDirection {
+//     Number(i32),
+//     Direction(String),
+// }
+
+// #[derive(Builder, Clone)]
+// pub struct ImgixRect {
+//     pub x: RectDirection,
+//     pub y: RectDirection,
+//     pub w: i32,
+//     pub h: i32,
+// }
+
+// #[derive(Builder, Clone)]
+// pub struct ImgixCrop {
+//     pub top: bool,
+//     pub bottom: bool,
+//     pub left: bool,
+//     pub right: bool,
+//     pub faces: bool,
+//     pub focalpoint: bool,
+//     pub edges: bool,
+//     pub entropy: bool,
+// }
+
+// #[derive(Builder, Clone)]
+// pub struct ImgixClientHints {
+//     pub width: bool,
+//     pub dpr: bool,
+//     // TODOeriaize as saveData?
+//     pub save_data: bool,
+// }
+
+pub struct ImgixUrl {
+    // auto: ImgixAuto,
+// rect: ImgixRect,
+// fit: ImgixFit,
+// crop: ImgixCrop,
+// ch: ImgixClientHints,
+// cs: ImgixClientHints,
+}
+
+impl ImgixUrl {
+    pub fn build(url: &str) -> ImgixUrlBuilder {
+        ImgixUrlBuilder {
+            params: Vec::new(),
+            url: url.into(),
+        }
+    }
+}
+
+pub struct ImgixUrlBuilder<'a> {
+    params: Vec<(&'a str, String)>,
+    url: String,
+}
+
+impl<'a> ImgixUrlBuilder<'a> {
+    pub fn finish(&self) -> String {
+        let qs = QString::new(self.params.to_owned());
+
+        format!("{}/?{}", self.url, qs)
+    }
+
+    /// Quality
+    pub fn q(&mut self, val: i32) -> &mut Self {
+        self.params.push(("q", val.to_string()));
+        self
+    }
+    /// Width
+    pub fn w(&mut self, val: i32) -> &mut Self {
+        self.params.push(("w", val.to_string()));
+        self
+    }
+    /// Height
+    pub fn h(&mut self, val: i32) -> &mut Self {
+        self.params.push(("h", val.to_string()));
+        self
+    }
+    pub fn dpr(&mut self, val: i32) -> &mut Self {
+        self.params.push(("dpr", val.to_string()));
+        self
+    }
+    pub fn bg(&mut self, val: &str) -> &mut Self {
+        self.params.push(("bg", val.into()));
+        self
+    }
+    pub fn blur(&mut self, val: i32) -> &mut Self {
+        self.params.push(("blur", val.to_string()));
+        self
+    }
+    pub fn faceindex(&mut self, val: i32) -> &mut Self {
+        self.params.push(("faceindex", val.to_string()));
+        self
+    }
+    pub fn facepad(&mut self, val: i32) -> &mut Self {
+        self.params.push(("facepad", val.to_string()));
+        self
+    }
+
+    /// Aspect Ratio
+    pub fn ar(&mut self, w: i32, h: i32) -> &mut Self {
+        self.params.push(("ar", format!("{}:{}", w, h)));
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn works() {
+        let url = ImgixUrl::build("https://foo.com")
+            .blur(40)
+            .q(40)
+            .w(300)
+            .ar(9, 1)
+            .finish();
+
+        assert_eq!(url, "https://foo.com/?blur=40&q=40&w=300&ar=9:1")
+    }
+}
