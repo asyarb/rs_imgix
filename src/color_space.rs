0ea05c4e41d1bf4259955e8ca76ff24a0b8cfc72
git <asyarb@hawaii.edu>
2020-08-08 13:55:46 -1000

    docs: color space docs

--- src/color_space.rs
+++ src/color_space.rs
@@ -1,7 +1,21 @@
+/// The `cs` parameter specifies the color space of the output image.
+///
+/// See [Imgix docs](https://docs.imgix.com/apis/url/format/cs) for more
+/// info.
+#[derive(Debug)]
 pub enum ImgixColorSpace {
+    /// Sets the image to the sRGB color space, the Internet standard.
     SRGB,
+
+    /// Sets the image to the Adobe RGB (1998) color space which provides
+    /// accurate color reproduction from screen to print.
     AdobeRGB1998,
+
+    /// Saves on file size by reducing the color space metadata. May cause a
+    /// slight shift in color values.
     TinySRGB,
+
+    /// Completely removes color space metadata for maximum size reduction.
     Strip,
 }
 
